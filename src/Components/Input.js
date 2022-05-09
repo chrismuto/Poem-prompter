7f013cc7e200872ecbedd2f5ff3a8a1e813cb3a0
chrismuto <chrismuto@gmail.com>
2022-05-09 14:22:33 -0700

    added input and response components, made sure they could be displayed through app.js

---
+++ src/Components/Input.js
@@ -0,0 +1,7 @@
+export default function Input() {
+    return (
+        <div>
+            test a bunch of text
+        </div>
+    );
+}
---
+++ src/Components/Responses.js
@@ -0,0 +1,7 @@
+export default function Responses() {
+    return (
+        <div>
+            test a bunch of text
+        </div>
+    );
+}
