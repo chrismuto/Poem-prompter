e2cfb731071a4bb571467b81d122b120a9e9cde2
chrismuto <chrismuto@gmail.com>
2022-05-19 11:59:17 -0700

    storing response text to local storage

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -5,32 +5,38 @@ const { Configuration, OpenAIApi } = require("openai");
 
 
 export default function Input() {
+
+  
   const configuration = new Configuration({
     apiKey: process.env.REACT_APP_API_KEY,
   });
-
+  
   const openai = new OpenAIApi(configuration);
   
   const [formState, setFormState] = useState({
     inputField: "",
   });
-
-  const [responseState, setResponseState] = useState({
-    response: "",
-  });
-
+  
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     if (name === "inputField") {
       setFormState({ ...formState, [name]: value });
     }
   };
-
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
     requestPoem(formState.inputField);
   };
-
+  
+  const localSave = (data) => {
+    let a = [];
+    a = JSON.parse(localStorage.getItem("prevAnswers")) || [];
+    a.push(data);
+    console.log(a);
+    localStorage.setItem("prevAnswers", JSON.stringify(a));
+  }
+  
   const requestPoem = (data) => {
     openai.createCompletion("text-curie-001", {
         prompt: "finish a story that starts with " + data,
@@ -41,29 +47,11 @@ export default function Input() {
         presence_penalty: 0,
       })
       .then((response) => {
-        console.log(response);
-        setResponseState({
-          ...responseState, response:`${response.data.choices[0].text}`,
-        });
+        console.log(response.data.choices[0].text);
+        localSave(response.data.choices[0].text);
       });
-    // try {
-    //   requestPoem({ variables: { ...formState } });
-    // } catch (err) {
-    //   console.error(err);
-    //   alert("an error has occurred, please try again");
-    // }
-    setFormState({ inputField: "" });
-    // console.log(data);
-    // fetch("https://api.openai.com/v1/engines/text-curie-001/completions?prompt=" + formState.inputField + "&instruction=text completion&max_tokens=5&temperature=0.1&echo=true/", {
-    //   method: "POST",
 
-    //   headers: {
-    //     "Access-Control-Allow-Credentials": "true",
-    //     "Content-Type": "application/json",
-    //     "Authorization": `Bearer ${process.env.REACT_APP_API_KEY}`,
-    //   },
-    //   body: JSON.stringify(data),
-    // });
+    setFormState({ inputField: "" });
   };
 
   return (
@@ -79,7 +67,7 @@ export default function Input() {
       />
       <div className="d-flex col-9 m-auto my-2 justify-content-end">
         <Button variant="primary" type="submit" onClick={handleSubmit}>
-          Primary
+          Send Prompt
         </Button>
       </div>
     </div>
