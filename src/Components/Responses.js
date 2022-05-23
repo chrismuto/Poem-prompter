7d9e20cd6e166df9ae43fd61e0c4f5eba1426d9a
chrismuto <chrismuto@gmail.com>
2022-05-23 10:22:02 -0700

    added ternary to prevent render error when no items are in stored array

--- src/Components/Responses.js
+++ src/Components/Responses.js
@@ -8,10 +8,6 @@ export default function Responses() {
     prevResponse: storedAnswers,
   });
 
-//   const storageUpdate = () => {
-//     setResponses({ ...responses, storedAnswers })
-// };
-
 window.addEventListener('storage', () => {
     storedAnswers = JSON.parse(localStorage.getItem("prevAnswers"));
     setResponses({ ...responses, prevResponse: storedAnswers });
@@ -22,11 +18,11 @@ console.log(responses);
   return (
     <div className="d-flex justify-content-center mt-5">
       <div name="prevResponse" className="d-flex-block justify-content-center col-10">
-        {responses.prevResponse.map((item) => (
+        {responses.prevResponse ? responses.prevResponse.map((item) => (
           <div key={Object.keys(item)} className="mx-5 my-3 p-2 responses">
             {item}
           </div>
-        ))}
+        )) : "No stories have been told"}
       </div>
     </div>
   );
